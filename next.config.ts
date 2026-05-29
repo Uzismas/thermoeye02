1d729df5da58556e6d3675f5fcdf7f430f9c3a83
Uzismas <papangkron.p6@gmail.com>
2026-05-29 08:16:03 +0700

    Fix next config conflict

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-<<<<<<< HEAD
-=======
   output: "export",
   basePath: "/thermoeye02",
->>>>>>> a3f57101bbb2c5a5d9134c0da9ce4f44911b1018
 };
 
 export default nextConfig;
