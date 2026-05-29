2d0291a366c57f7fa629811f22fb19c481c10f40
Uzismas <papangkron.p6@gmail.com>
2026-05-29 08:25:39 +0700

    Remove static export config

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  output: "export",
-  basePath: "/thermoeye02",
 };
 
 export default nextConfig;
